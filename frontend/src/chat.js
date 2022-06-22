786fabee6728e350f2615d116d10ee4c2c0998b8
LuizFelipe883 <luizfelipedesouza94@gmail.com>
2022-06-22 18:50:58 -0300

    customizando front

--- frontend/src/chat.js
+++ frontend/src/chat.js
@@ -1,22 +1,72 @@
 import React from 'react';
 
-const Chat = () => {
+function Chat() {
 
   return (
-  
-      <section id="mesa">
-        <div id="cartas" >
-        <h1>Luiz Felipe</h1>
+
+    <div>
+      <div class="collapse" id="navbarToggleExternalContent">
+        <div class="bg-dark p-4">
+          <h5 class="text-white h4">Menu</h5>
+
+          <button id="btn-players" type="button" class="btn btn-info m-2">Undefined</button>
+
+          <button id="btn-criarPartida" type="button" class="btn btn-info m-2">Undefined</button>
+
+          <button id="btn-limparMesa" type="button" class="btn btn-info m-2">Undefined</button>
+
+          <button id="btn-limparMesa" type="button" class="btn btn-info m-2">Undefined</button>
+
+          <button id="btn-limparMesa" type="button" class="btn btn-info m-2">Undefined</button>
+
+          <button id="btn-limparMesa" type="button" class="btn btn-info m-2">Undefined</button>
+
+          <button id="btn-limparMesa" type="button" class="btn btn-info m-2">Undefined</button>
+
+          <button id="btn-limparMesa" type="button" class="btn btn-info m-2">Undefined</button>
+
+          <button id="btn-limparMesa" type="button" class="btn btn-info m-2">Undefined</button>
+
+          <button id="btn-limparMesa" type="button" class="btn btn-info m-2">Undefined</button>
+
+          <button id="btn-limparMesa" type="button" class="btn btn-info m-2">Undefined</button>
+
+        </div>
+      </div>
+      <nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            Menu</button>
+        </div>
+      </nav>
+
+      <div id="mesa">
+
+        <div id="carta1">
+
+          <div id="frenteCarta1">frente1</div>
+          <div id="versoCarta1">verso1</div>
+
         </div>
 
-        <div id="cartas" >
-        <h1>Luiz Felipe</h1>
+        <div id="carta2">
+
+          <div id="frenteCarta2">frente2</div>
+          <div id="versoCarta2">verso2</div>
+
         </div>
 
-        <div id="cartas" >
-        <h1>Luiz Felipe</h1>
+        <div id="carta3">
+
+          <div id="frenteCarta3">frente3</div>
+          <div id="versoCarta3">verso3</div>
+
         </div>
-      </section>
+
+      </div>
+
+    </div>
 
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.css';
 import Main from './chat';
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById("pagInicial"));
+const root = ReactDOM.createRoot(document.getElementById("body"));
 root.render(
   <React.StrictMode>
     <Main />
   </React.StrictMode>
 )
+
+reportWebVitals();
